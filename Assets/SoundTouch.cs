360de90e3874db54812d53a1ec1d3eeb5d596d84
Jatin Agrawal <jatinagrawal0801@gmail.com>
2024-02-03 22:29:35 -0800

    high score feature enabled

--- Assets/SoundTouch.cs
+++ Assets/SoundTouch.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using System.Collections;
 using System.Diagnostics;
-
+using TMPro;
+using System;
+using System.IO;
 public class Victory : MonoBehaviour
 {
     public AudioSource startMusic;
@@ -9,14 +11,30 @@ public class Victory : MonoBehaviour
     public AudioSource backgroundMusic;
     private bool isVictory = false;
     public LevelManager levelManager;
-
+    public TextMeshProUGUI time;
     void Start()
     {
-
         levelManager.isRunning = false;
         StartGame();
     }
 
+     bool IsTime1LessThanTime2(string time1, string time2)
+    {
+        TimeSpan ts1 = TimeSpan.ParseExact(time1, "mm\\:ss", null);
+        TimeSpan ts2 = TimeSpan.ParseExact(time2, "mm\\:ss", null);
+        return ts1 < ts2;
+    }
+    public void ToJSON(string path,string time)
+    {
+        string jsonContent = File.ReadAllText(path);
+        if(jsonContent==""){
+            File.WriteAllText(path, time);
+        }
+        else if (IsTime1LessThanTime2(time, jsonContent.Replace("\"", string.Empty)))
+        {
+            File.WriteAllText(path, time);
+        }
+    }
     void OnCollisionEnter(Collision collision)
     {
         UnityEngine.Debug.Log(collision.collider.name.ToString());
@@ -27,6 +45,7 @@ public class Victory : MonoBehaviour
             backgroundMusic.Stop();
             // Play victory sound
             audioSource.Play();
+            ToJSON("Assets/Scores.txt",time.text.ToString());
             levelManager.isRunning = false;
 
             Invoke("StartGame", 15);
